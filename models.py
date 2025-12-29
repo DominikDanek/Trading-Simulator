81868d06f0ae3d315b292c433ae0ed91f0500838
DominikDanek <danek.dominik@yahoo.com>
2025-12-29 14:09:31 +0000

    Update models.py
    Lot of debugging, had to remove the get_ma and get_rsi functions from the other two classes, I learned that python has issues choosing which function from which class to call, so It wouldnt run at all as a result, so I just put the rsi and ma functionality inside the logistic regression class instead so its no longer a subclass of moving average crossover and rsi mean reversion.

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Model:
         self.s = s
         self.e = e
         self.t = t
-        self.data = yf.download(self.t, start=self.s, end=self.e)
+        self.data = yf.download(self.t, start=self.s, end=self.e,auto_adjust=False)
 
     def plot_returns(self, data):
 
@@ -27,17 +27,14 @@ class Model:
         return (total_strategy_return*100).round(2)
     
 class moving_average_crossover(Model):
-    def get_ma(self,data,short_window, long_window):
-
+    def get_returns(self, short_window, long_window):
+        data = self.data
         data['Daily Return'] = data['Close'].pct_change()
 
         data['SMA_short'] = data['Close'].rolling(window=short_window).mean()
         data['SMA_long'] = data['Close'].rolling(window=long_window).mean()
         data['SMA_change'] = data['SMA_short']- data['SMA_long']
-        return data
-    def get_returns(self, short_window, long_window):
 
-        data = self.get_ma(self.data,short_window,long_window)
         data['Signal'] = 0
         data.loc[data['SMA_short'] > data['SMA_long'], 'Signal'] = 1
         data.loc[data['SMA_short'] < data['SMA_long'], 'Signal'] = -1
@@ -51,7 +48,9 @@ class moving_average_crossover(Model):
         return data
     
 class rsi_mean_reversion(Model):
-    def get_rsi(self, data, period):
+    def get_money(self,period,overbought,oversold):
+        data = self.data
+
         data["Daily Return"] = data["Close"].pct_change()
 
         delta = data['Close'].diff()
@@ -59,13 +58,8 @@ class rsi_mean_reversion(Model):
         loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
 
         rs = gain / loss
-        data["RSI"] = 100 - (100 / (1 + rs)) #calculate rsi
-        return data
-    
-    def get_returns(self,period,overbought,oversold):
-        data = self.data
 
-        data = self.get_rsi(data, period)
+        data["RSI"] = 100 - (100 / (1 + rs)) #calculate rsi
 
         data["Signal"] = 0
         data.loc[data["RSI"] < oversold, "Signal"] = 1
@@ -80,28 +74,43 @@ class rsi_mean_reversion(Model):
 
         return data
     
-class logistic_regression(Model, rsi_mean_reversion, moving_average_crossover):
-    def __init__(self):
+class logistic_regression(Model):
+    def __init__(self,s,e,t):
+        super().__init__(s,e,t)
         #trained model
         self.model = Pipeline([
             ("scaler", StandardScaler()),
             ("lr", LogisticRegression(solver="lbfgs"))
         ])
-        self.data = self.data[["Close", "Volume"]].dropna()
-
+        self.data = yf.download(self.t, start=self.s, end=self.e)[["Close", "Volume"]].dropna()
+        print('hello', len(self.data))
+        
     def get_features(self,period,short_window,long_window):
-        data = self.data
-        data['returns'] = data['Close'].pct_change()
+        
+        data = self.data.copy()
+        data['Daily Return'] = data['Close'].pct_change()
+        data['returns'] = data['Daily Return']
         data["volatility"] = data["returns"].rolling(period).std()
+
         #append moving average diff
-        data = self.get_ma(data,short_window,long_window)
+        data['SMA_short'] = data['Close'].rolling(window=short_window).mean()
+        data['SMA_long'] = data['Close'].rolling(window=long_window).mean()
+        data['SMA_change'] = data['SMA_short']- data['SMA_long']
+
         #append rsi
-        data = self.get_rsi(data, period)
-        data["target"] = (data["Close"].shift(-1) > data["Close"])
+        delta = data['Close'].diff()
+        gain = (delta.where(delta > 0, 0)).rolling(window=period).mean() #getting the average gain and loss over input period
+        loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
+        rs = gain / loss
+        data["RSI"] = 100 - (100 / (1 + rs))
+        data["target"] = (data["Close"].shift(-1) > data["Close"]).astype(int)
+
         data = data.dropna()
+        self.data = data
+        print("Rows after features B:", len(self.data))
 
     def train(self, test_split):
-        data = self.data
+        data = self.data.copy()
 
         features = [
             "returns",
@@ -113,11 +122,13 @@ class logistic_regression(Model, rsi_mean_reversion, moving_average_crossover):
 
         split = int(len(data) * test_split)
         #inputting training data for the sckit library
-        x_train = data[features].iloc[:split]
+        X_train = data[features].iloc[:split]
         y_train = data["target"].iloc[:split]
-        self.model.fit(x_train, y_train)
+        self.model.fit(X_train, y_train)
         self.features = features
         self.split_index = split
+
+        self.data = data
 
     def predict_probabilities(self):
         data = self.data
@@ -126,21 +137,27 @@ class logistic_regression(Model, rsi_mean_reversion, moving_average_crossover):
         probs = self.model.predict_proba(X)[:, 1]
 
         data["prob_up"] = probs
+        return data
 
     def strategy(self, upper, lower):
         data = self.predict_probabilities()
+        print(data)
 
         data["Signal"] = 0
         data.loc[data["prob_up"] > upper, "Signal"] = 1
         data.loc[data["prob_up"] < lower, "Signal"] = -1
-        
-        data["Position"] = (data["Signal"].replace(0, np.nan).ffill().fillna(0).shift(1))
 
-        data["Strategy Return"] = data["Position"] * data["Daily Return"]
+        print(data)
+        data["Strategy Return"] = data["Signal"] * data["Daily Return"]
 
         data["Cumulative Market Return"] = (1 + data["Daily Return"]).cumprod()
         data["Cumulative Strategy Return"] = (1 + data["Strategy Return"]).cumprod()
+
         return data
+
+
+
+
 
 
 
